cc539ddd537fa8abe37326a6faec65d7ebf8c42d
oscarbolano <79919753+oscarbolano@users.noreply.github.com>
2021-03-12 13:58:19 -0500

    Create ejemploFecha.js

---
+++ nodejs/ejemploFecha.js
@@ -0,0 +1,12 @@
+let edad;
+function calcularEdad(fecha) {
+    let hoy = new Date();
+    let fechaNac = new Date(fecha);
+    let edad = hoy.getFullYear() - fechaNac.getFullYear();
+    let m = hoy.getMonth() - fechaNac.getMonth();
+    if (m < 0 || (m === 0 && hoy.getDate() < fechaNac.getDate())) {
+        edad--;
+    }
+    return edad;
+}
+console.log(calcularEdad("10/12/1992"));
